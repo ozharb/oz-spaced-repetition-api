91586e4e43dd5a777f0366131215d508edfd65b9
Ozzie Harb <ozharb@gmail.com>
2021-03-01 10:46:28 -0800

    ssl

--- postgrator-config.js
+++ postgrator-config.js
@@ -11,5 +11,6 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
   ? process.env.TEST_DATABASE_URL
   : process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production',
  
 }
