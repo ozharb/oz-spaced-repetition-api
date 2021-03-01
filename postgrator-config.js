1cda3d60d736f1ea04a90bdfc58c484083049d39
Ozzie Harb <ozharb@gmail.com>
2021-03-01 09:25:38 -0800

    postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -8,5 +8,8 @@ module.exports = {
   "database": process.env.MIGRATION_DB_NAME,
   "username": process.env.MIGRATION_DB_USER,
   "password": process.env.MIGRATION_DB_PASS,
+  "connectionString": (process.env.NODE_ENV === 'test')
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL,
   ssl: process.env.NODE_ENV === 'production',
 }
